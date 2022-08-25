f8a421e5009818ed4ce4e1d2b35756962e85212b
BillyPhan <phanvanthong1994@gmail.com>
2022-08-25 11:50:26 +0700

    Lab 1 JS

---
+++ script.js
@@ -0,0 +1,48 @@
+const country = 'Viet Nam';
+const continent = 'Asian';
+const population = 98;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// console.log(country + " is located on the " + continent + " continent" + " with " + population + " million people ");
+
+// console.log(`${country} is located on the ${continent} continent with ${population} million people`);
+
+
+
+
+// int = 10;
+// a = "string";
+
+// let a = 12;
+// console.log(a, typeof a);
+
+// a = 'Hello';
+// console.log(a, typeof a);
+
+// let b = 'Hello2';
+// console.log(b, typeof b);
+// b = 100;
+// console.log(b, typeof b);
+
+
+// let value = 12;
+// value = 14;
+// console.log(value);
+
+// const values = 100;
+// console.log(values);
+
+
+// let num = 12;
+// // console.log(!true && !false);
+
+// if (num >= 12) {
+//     console.log(num > 10);
+//     console.log(num > 15);
+// }
+
+
+// console.log(12 + 3 + 10 * 3);
